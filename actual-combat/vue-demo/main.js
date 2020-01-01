3a59dd84fabc040111a3325f4aa7a03d5f1de9d0
yukai <yukai1102huadian@163.com>
2020-01-01 21:01:59 +0800

    调试vue项目的webpack配置

--- actual-combat/vue-demo/main.js
+++ actual-combat/vue-demo/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from "./src/app.vue";
+import "./test.css"
 
 new Vue({
     el:"#app",
--- actual-combat/vue-demo/webpack.config.js
+++ actual-combat/vue-demo/webpack.config.js
@@ -1,14 +1,16 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const { resolve, join } = require("path")
 const { VueLoaderPlugin } = require("vue-loader");
-console.log(join(__dirname, './dist/assets/lib'))
-console.log(resolve(__dirname, './src/assets/lib'))
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const config = {
-  entry: "./main.js",
+  entry: {
+    main: "./main.js",
+  },
   output: {
     path: resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist"
+    filename: "assets/js/bundle.js",
+    // publicPath: "/dist"
   },
   mode: "production",
   // 使用引入的vue，就不用把vue打包了
@@ -18,44 +20,55 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 8192,
+              outputPath: 'assets/img',
+              name: "[hash:8].[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
         test: /\.vue/,
         use: ["vue-loader"]
       },
       {
-        // 参考https://vue-loader.vuejs.org/zh/migrating.html#%E5%80%BC%E5%BE%97%E6%B3%A8%E6%84%8F%E7%9A%84%E4%B8%8D%E5%85%BC%E5%AE%B9%E5%8F%98%E6%9B%B4
         test: /\.css$/,
-        oneOf: [
-          // 这里匹配 `<style module>`
+        use: [
           {
-            resourceQuery: /module/,
-            use: [
-              'vue-style-loader',
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true,
-                  localIdentName: '[local]_[hash:base64:5]'
-                }
-              }
-            ]
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../'
+            }
           },
-          // 这里匹配普通的 `<style>` 或 `<style scoped>`
-          {
-            use: [
-              'vue-style-loader',
-              'css-loader'
-            ]
-          }
-        ]
+          "css-loader",
+        ],
       }
     ]
   },
   plugins: [
+    // if you have plugins that make use of it, html-webpack-plugin should be ordered first before any of the integrated plugins.
+    new HtmlWebpackPlugin({
+      title: "自定义webpack",  // 页面title
+      favicon: resolve(__dirname, "./favicon.ico"),  // 打包前的favicon路径,
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./assets/css/index.css",
+      chunkFilename: "./assets/css/[id].css",
+
+    }),
+    // 压缩css
+    // new OptimizeCSSAssetsPlugin({}),
     new VueLoaderPlugin(),
     new CopyPlugin([{
       from: resolve(__dirname, './src/assets/lib'),
       to: join(__dirname, './dist/assets/lib')
-    }])
+    }]),
   ]
 }
 module.exports = config;
